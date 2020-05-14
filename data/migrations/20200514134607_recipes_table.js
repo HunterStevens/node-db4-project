931dc1c78121e1d23ca2a706ebeadc1453a4f009
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-14 16:49:21 -0600

    finished migrations creating the tables

--- data/migrations/20200514134607_recipes_table.js
+++ data/migrations/20200514134607_recipes_table.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('recipes', rec =>{
+        rec.increments();
+        rec.string('name', 255).notNullable().unique();
+    })
+    .createTable('ingredients', ing=>{
+        ing.increments();
+        ing.string('name', 255).notNullable().unique();
+    })
+    .createTable('steps_recipes', steps=>{
+        steps.increments();
+        steps.integer('step_number').notNullable();
+        steps.string('instructions').notNullable().unique();
+        steps.integer('recipe_id').notNullable()
+        .references('recipes.id');
+    })
+    .createTable('recipes_ingredients', reIN=>{
+        reIN.increments();
+        reIN.integer('recipe_id').notNullable()
+        .references('recipes.id')
+        reIN.integer('ingredient_id').notNullable()
+        .references('ingredients.id')
+        reIN.string('quantity').notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('recipes_ingredients')
+  .dropTableIfExists('steps_recipes')
+  .dropTableIfExists('ingredients')
+  .dropTableIfExists('recipes');
 };
